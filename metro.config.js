b98d72152d164fb4af6b1f5ee37a81ff9efc4a3f
Cedric van Putten <github@cedric.dev>
2023-09-21 17:50:16 +0200

    fix: manually patch known metro-config issues

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,13 @@ const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
 });
 
+// Enable pnpm symlinks
+config.resolver.unstable_enableSymlinks = true;
+
+// Metro config issue, resolve `metro-runtime` path relative from `react-native`
+config.transformer.asyncRequireModulePath = require.resolve(
+  'metro-runtime/src/modules/asyncRequire',
+  { paths: [require.resolve('react-native/package.json')] },
+);
+
 module.exports = config;
